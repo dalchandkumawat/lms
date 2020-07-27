42463410d527cc1d804e1af4472948f0ed575703
Dalchand Kumawat <dalchandkumawat9@gmail.com>
2020-07-27 11:43:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ process.on('unhandledRejection', (error, promise) => {
     console.log(' The error was: ', error);
 });
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path=require('path')
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
 app.listen(PORT, () => {
     console.log("Listening at ",PORT);
 });
